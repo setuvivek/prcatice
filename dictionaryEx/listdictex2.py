19fc18021b0d7fbc4614fb8ea08ef53e8237782e
Jatin11111 <jatin.setuconsulting@gmail.com>
2024-02-08 18:31:12 +0530

    add my dictionary exercises

---
+++ dictionaryEx/listdictex2.py
@@ -0,0 +1,120 @@
+mylist1 = [{'product': 'furn_0789', 'quantity': 16.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_6666', 'quantity': 16.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com08', 'quantity': 18.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com07', 'quantity': 500.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com10', 'quantity': 22.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com11', 'quantity': 33.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com12', 'quantity': 26.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com13', 'quantity': 30.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_0096', 'quantity': 45.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_0097', 'quantity': 50.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_0098', 'quantity': 55.0, 'warehouse': 'san francisco'},
+          {'product': 'desk0004', 'quantity': 60.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7800', 'quantity': 60.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_0269', 'quantity': 10.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_1118', 'quantity': 2.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_8855', 'quantity': 80.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com07', 'quantity': 200.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_8888', 'quantity': 45.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com06', 'quantity': 75.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_5555', 'quantity': 50.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7777', 'quantity': 35.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7888', 'quantity': 125.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com11', 'quantity': 120.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_0789', 'quantity': 60.0, 'warehouse': 'chicago'},
+          {'product': 'furn_6666', 'quantity': 60.0, 'warehouse': 'chicago'},
+          {'product': 'e-com08', 'quantity': 18.0, 'warehouse': 'chicago'},
+          {'product': 'e-com07', 'quantity': 500.0, 'warehouse': 'chicago'},
+          {'product': 'e-com18', 'quantity': 22.0, 'warehouse': 'chicago'},
+          {'product': 'e-com11', 'quantity': 33.0, 'warehouse': 'chicago'},
+          {'product': 'e-com12', 'quantity': 26.0, 'warehouse': 'chicago'},
+          {'product': 'e-com13', 'quantity': 30.0, 'warehouse': 'chicago'},
+          {'product': 'furn_0096', 'quantity': 45.0, 'warehouse': 'chicago'},
+          {'product': 'furn_0097', 'quantity': 50.0, 'warehouse': 'chicago'},
+          {'product': 'furn_0098', 'quantity': 55.0, 'warehouse': 'chicago'},
+          {'product': 'desk0004', 'quantity': 60.0, 'warehouse': 'chicago'},
+          {'product': 'furn_7800', 'quantity': 60.0, 'warehouse': 'chicago'},
+          {'product': 'furn_0269', 'quantity': 18.0, 'warehouse': 'chicago'},
+          {'product': 'furn_1118', 'quantity': 2.0, 'warehouse': 'chicago'},
+          {'product': 'furn_8855', 'quantity': 80.0, 'warehouse': 'chicago'},
+          {'product': 'e-com07', 'quantity': 200.0, 'warehouse': 'chicago'},
+          {'product': 'furn_8888', 'quantity': 45.0, 'warehouse': 'chicago'},
+          {'product': 'e-com06', 'quantity': 75.0, 'warehouse': 'chicago'},
+          {'product': 'furn_5555', 'quantity': 50.0, 'warehouse': 'chicago'},
+          {'product': 'furn_7777', 'quantity': 35.0, 'warehouse': 'chicago'},
+          {'product': 'furn_7888', 'quantity': 150.0, 'warehouse': 'chicago'},
+          {'product': 'e-com11', 'quantity': 120.0, 'warehouse': 'chicago'},
+          {'product': 'furn_8888', 'quantity': 50.0, 'warehouse': 'chicago'},
+          {'product': 'e-com18', 'quantity': 25.0, 'warehouse': 'chicago'},
+          {'product': 'furn_7777', 'quantity': 45.0, 'warehouse': 'chicago'},
+          {'product': 'furn_8888', 'quantity': 50.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com10', 'quantity': 25.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7777', 'quantity': 45.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7888', 'quantity': 75.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_8855', 'quantity': 15.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_8888', 'quantity': 45.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com06', 'quantity': 75.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_8855', 'quantity': 15.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_6666', 'quantity': 10.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7777', 'quantity': 100.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com07', 'quantity': 100.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com07', 'quantity': 100.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7777', 'quantity': 80.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7800', 'quantity': 16.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7800', 'quantity': 32.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com07', 'quantity': 50.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_6666', 'quantity': 20.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com07', 'quantity': 80.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com07', 'quantity': 80.0, 'warehouse': 'san francisco'},
+          {'product': 'desk0005', 'quantity': 65.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_7800', 'quantity': 8.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_6666', 'quantity': 5.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com11', 'quantity': 5.0, 'warehouse': 'san francisco'},
+          {'product': 'furn_6666', 'quantity': 5.0, 'warehouse': 'san francisco'},
+          {'product': 'e-com11', 'quantity': 10.0, 'warehouse': 'san francisco'},
+          {'product': 'desk0006', 'quantity': 70.0, 'warehouse': 's francisco'}]
+
+dict={}
+for i in mylist1:
+    w = i.get('warehouse')
+    p = i.get('product')
+    q = i.get('quantity')
+    if w in dict:
+        flag = 1
+        for j in dict[w]:
+            if j['product'] == i['product']:
+                j['quantity'] += i['quantity']
+                flag = 0
+        if flag:
+                a = dict[w]
+                a.append({'product':p,'quantity':q})
+    else:
+        dict.update({w:[{'product':p,'quantity':q}]})
+
+print(dict)
+
+dict = {}
+flag = 1
+for i in mylist1:
+
+    w = i.get('warehouse')
+    p = i.get('product')
+    q = i.get('quantity')
+
+    if w in dict:
+        flag=1
+        for j in dict[w]:
+
+            if j['product'] == i['product']:
+                j['quantity'] += i['quantity']
+                flag = 0
+        # print(dict[w])
+        if flag:
+                dict.get(w).append({'product': p, 'quantity': q})
+        # v = dict[w]
+
+        # v = dict[w],
+    else:
+     dict.update({w: [{'product':p,'quantity':q}]})
+#
+# print(dict)
