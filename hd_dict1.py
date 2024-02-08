df6c35f39ac802adc66e87e0fc66182d00ec2853
hemangi-setu <hemangi.setuconsulting@gmail.com>
2024-02-08 18:22:42 +0530

    hd_dict1.py

---
+++ hd_dict1.py
@@ -0,0 +1,24 @@
+#Add two dict , if both keys are present in each dict than add
+# otherwise remaining key should be printed in the final dict
+# def merge(dict1,dict2):
+#     return(dict2.update(dict1))
+dict1 = {
+    "a":10,
+    "b":20,
+    "c":30
+}
+dict2 = {
+    "a":5,
+    "b":15,
+    "d":10
+}
+e = {**dict1, **dict2}  #merge dictionary
+
+for key in dict1:
+    if key in dict2:
+        e[key] = dict2[key] + dict1[key]
+    else:
+        pass
+
+print(e)
+
