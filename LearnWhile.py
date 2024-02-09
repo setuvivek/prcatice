06b362373ce7cd10069104b632376d4a5beded67
setu <Biren.setuconsulting@gmail.com>
2024-02-09 17:58:09 +0530

    [add] concept related to loops (for , while)

---
+++ LearnWhile.py
@@ -0,0 +1,18 @@
+#while loop
+
+a=1
+while a < 10:
+    print(a)
+    a += 1
+    if a == 6:
+        break
+print()
+
+a=1
+print(a)
+while a < 10:
+    a+=1
+    if a == 6:
+        continue
+    print(a)
+print()
