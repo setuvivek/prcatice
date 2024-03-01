5f2ad106ea7d8bfda314949e521986d618411124
BirenVChauhan <biren.setuconsulting@gmail.com>
2024-03-01 17:51:30 +0530

    adding sh file of commands to push pull

---
+++ git.sh
@@ -0,0 +1,9 @@
+git checkout staging
+git pull origin staging
+git pull origin (BranchName)
+git push origin staging
+git checkout main
+git pull origin main
+git pull origin (BranchName)
+git push origin main
+
