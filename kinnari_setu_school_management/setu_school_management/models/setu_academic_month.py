60bff6d8a7f2aaaf1be6dace5898463222915827
kinnari <kinnari.setuconsulting@gmail.com>
2024-03-12 17:24:17 +0530

    add setu school

--- kinnari_setu_school_management/setu_school_management/models/setu_academic_month.py
+++ kinnari_setu_school_management/setu_school_management/models/setu_academic_month.py
@@ -1,4 +1,6 @@
-from odoo import fields,models
+from odoo import fields,models, api
+# from odoo.exceptions import ValidationError
+
 
 class SetuAcademicMonth(models.Model):
     _name = "setu.academic.month"
@@ -9,10 +11,24 @@ class SetuAcademicMonth(models.Model):
     date_stop = fields.Date(string="Stop Date")
     academic_year_id = fields.Many2one('setu.academic.year',string="Year")
 
-    _sql_constraints = [
-        # Partial constraint, complemented by unique index (see below). Still
-        # useful to keep because it provides a proper error message when a
-        # violation occurs, as it shares the same prefix as the unique index.
-        ('name_compulsory', 'CHECK(name IS NOT NULL)', 'Name should required'),
-        ('code', 'CHECK(code>=0)', 'Code cannot be negative.'),
-    ]
+
+
+    # _sql_constraints = [
+    #     # Partial constraint, complemented by unique index (see below). Still
+    #     # useful to keep because it provides a proper error message when a
+    #     # violation occurs, as it shares the same prefix as the unique index.
+    #     ('name_compulsory', 'CHECK(name IS NOT NULL)', 'Name should required'),
+    #     ('name_unique', 'unique(name)', "Name Must Be Unique."),
+    #     ('code', 'CHECK(code>0)', 'Code cannot be negative.'),
+    # ]
+
+    # @api.constrains('date_start', 'date_stop')
+    # def _constrains_dates_(self):
+    #     for rec in self:
+    #         if rec.date_start and rec.date_stop and rec.date_start > rec.date_stop:
+    #             raise ValidationError(_('The stop date cannot be earlier than the start date. ',))
+    #
+    # @api.constrains('code')
+    # def _check_code_(self):
+    #     if not self.code:
+    #         raise ValidationError(_('Missing code'))
--- kinnari_setu_school_management/setu_school_management/models/setu_teacher.py
+++ kinnari_setu_school_management/setu_school_management/models/setu_teacher.py
@@ -1,9 +1,10 @@
-from odoo import fields, models,api
+from odoo import fields, models,api, _
+from odoo.exceptions import ValidationError
 
 class SetuTeacher(models.Model):
     _name = "setu.teacher"
 
-    name = fields.Char(string="Name" , required=True , copy=False)
+    name = fields.Char(string="Name")
     address = fields.Char(string="Address")
     city = fields.Char(string="City")
     state = fields.Char(string="State")
@@ -11,13 +12,16 @@ class SetuTeacher(models.Model):
     mobile = fields.Char(string="mobile" , help="Enter mobile Number" , size=10)
     phone = fields.Char(string="Phone", help="Enter Phone Number" , size=10)
     standard_id = fields.Many2one('setu.class', string="Responsibility of Academic Class")
+    division_id = fields.Many2one('setu.standard.division', string="Division")
+    medium_id = fields.Many2one('setu.standard.medium', string="Medium")
+    school_id = fields.Many2one('setu.school', string="Campus")
     subject_ids = fields.Many2many('setu.subject', 'setu_teachers_subjects', string="Subject")
-    school_id = fields.Many2one('setu.school' , string="Campus")
-    student_ids = fields.Many2many('setu.student','setu_teachers_students' , string="Students")
+    student_ids = fields.One2many('setu.student','teacher_id', string="Students")
     home_address = fields.Char(string="Home Address")
     street = fields.Char(string="Street")
     country = fields.Char(string="Country")
     zip = fields.Char(string="ZIP")
+    is_teacher = fields.Boolean(string="Is teacher")
 
 
 
@@ -30,3 +34,14 @@ class SetuTeacher(models.Model):
         res = super(SetuTeacher, self).create(vals_list)
         return res
 
+
+    _sql_constraints = [
+        ('name_compulsory', 'CHECK(name IS NOT NULL)', 'Name should required'),
+        ('name_unique', 'unique(name)', "Name Must Be Unique."),
+    ]
+
+    @api.constrains('zip')
+    def _verify_zip(self):
+        for rec in self:
+            if rec.zip and not rec.zip.isdigit():
+                raise ValidationError(_("The zip must be a sequence of digits."))
