64d71617feea21b3e841cb54178235be71646c8f
hemangi-setu <hemangi.setuconsulting@gmail.com>
2024-03-29 18:33:10 +0530

    [Odoo Module]

--- hd_odoo_module/sales_extended/models/sale_order.py
+++ hd_odoo_module/sales_extended/models/sale_order.py
@@ -9,8 +9,9 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         for order in self:
             total_price_subtotal = sum(orders.price_subtotal for orders in order.order_line)
-            if total_price_subtotal > order.partner_id.setu_credit_limit:
+            if order.partner_id.setu_credit_limit and total_price_subtotal > order.partner_id.setu_credit_limit and order.partner_id==order.partner_id:
                 raise ValidationError("Customer does not exceeds credit limit....")
+        return super(SaleOrder, self).action_confirm()
 
     # @api.constrains('order_line')
     # def check_credit_line(self):
