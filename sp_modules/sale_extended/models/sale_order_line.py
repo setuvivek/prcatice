3b6501ab4c340b4535b2eb77bd81fe9a1a0f88b1
setu-shivam <shivam.setuconsulting@gmail.com>
2024-04-05 18:34:23 +0530

    [05/04/2024]

--- sp_modules/sale_extended/models/sale_order_line.py
+++ sp_modules/sale_extended/models/sale_order_line.py
@@ -4,9 +4,32 @@ from odoo import fields, models, api
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    extra_price=fields.Integer(string='Extra Price')
+    extra_price = fields.Integer(string='Extra Price')
+
+    # @api.model
+    # def write(self,vals):
+    #     # vals.update({"price_unit": vals.get('price_unit')+=vals.get('extra_price')})
+    #     # self.price_unit+=self.extra_price
+    #     return super(SaleOrderLine,self).write(vals)
+
+    # def _convert_to_tax_base_line_dict(self):
+    #     self.env['account.tax']._convert_to_tax_base_line_dict(
+    #         self,
+    #         partner=self.order_id.partner_id,
+    #         currency=self.order_id.currency_id,
+    #         product=self.product_id,
+    #         taxes=self.tax_id,
+    #         price_unit=self.price_unit + self.extra_price,
+    #         quantity=self.product_uom_qty,
+    #         discount=self.discount,
+    #         price_subtotal=self.price_subtotal,
+    #     )
+    #     return super(SaleOrderLine,self)._convert_to_tax_base_line_dict()
 
 
-
+        # for order in self:
+        #     order.amount_untaxed += order.extra_price
+        # 
+        # return super(SaleOrderLine, self)._compute_amounts()
 
 
