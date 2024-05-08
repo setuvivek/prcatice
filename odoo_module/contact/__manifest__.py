38398947ce46399362c158c1d68f27c6c985552c
hemangi-setu <hemangi.setuconsulting@gmail.com>
2024-05-08 18:03:42 +0530

    Custom[Contact] Module

---
+++ odoo_module/contact/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*
+{
+    'name': 'Contact',
+    'version':'16.1',
+    'data':[
+        'security/ir.model.access.csv',
+        'views/city_views.xml',
+        'views/state_views.xml',
+        'views/country_views.xml',
+        'views/contact_partner_views.xml'
+    ],
+}
